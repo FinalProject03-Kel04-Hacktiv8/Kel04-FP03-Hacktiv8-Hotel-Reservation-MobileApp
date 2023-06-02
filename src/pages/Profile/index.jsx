03658225d646e57569ebe31cbfc19152526247bd
ekialfani <ekialfani15@gmail.com>
2023-06-02 22:27:04 +0800

    add: adding a card component and updating the profile page.

--- src/pages/Profile/index.jsx
+++ src/pages/Profile/index.jsx
@@ -1,13 +1,49 @@
-import { View, Text } from "react-native";
+import { View, Text, Image, FlatList } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StatusBar } from "expo-status-bar";
+import Card from "../../components/Card";
+import { useSelector } from "react-redux";
 
 export default function Profile() {
+  const user = useSelector((state) => state.auth.user);
+
   return (
-    <SafeAreaView className="flex-1 justify-center px-2">
-      <View className="relative border-2 h-80">
-        <Text className="font-bold absolute bottom-0">Profile</Text>
+    <SafeAreaView>
+      <View className="p-5">
+        <View className="bg-white p-3 flex-row w-full rounded-lg">
+          <View className="w-16 h-16 border-2 border-slate-600 mr-5">
+            <Image
+              className="w-full h-full"
+              source={{
+                uri: "https://media.istockphoto.com/id/1225790722/vector/user-profile-icon-vector-avatar-portrait-symbol-flat-shape-person-logo-web-and-mobile.jpg?s=170667a&w=0&k=20&c=31-8Hn2HeXSkhq050QQ1LKs7BclsmupgwM50Gt3WEMU="
+              }}
+            />
+          </View>
+          <View>
+            <Text className="font-semibold text-lg">
+              {`${user?.firstName} ${user?.lastName}`}
+            </Text>
+            <Text className="text-slate-500">
+              {user?.email}
+            </Text>
+          </View>
+        </View>
+
+        <Text 
+          className="mb-3 mt-10 text-lg font-medium">
+          Boking History
+        </Text>
+
+        <FlatList
+          data={[
+            {title: "garden Hotel"}, 
+            {title: "Mawar Hotel"}, 
+            {title: "Hora Umum Hotel"}
+          ]}
+          renderItem={({ item }) => <Card item={item} />}
+          keyExtractor={item => item.title}
+        />
         <StatusBar style="auto" />
       </View>
     </SafeAreaView>
