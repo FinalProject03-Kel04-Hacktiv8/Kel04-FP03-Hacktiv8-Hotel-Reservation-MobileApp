1d54e5d58a8bdc20665b9e62085a767840fb066c
ekialfani <ekialfani15@gmail.com>
2023-06-04 09:55:36 +0800

    feat: add redux persist configuration

--- App.jsx
+++ App.jsx
@@ -3,17 +3,20 @@ import Home from "./src/pages/Home";
 import { PaperProvider } from "react-native-paper";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
-import { store } from "./src/redux/store/store";
+import { store, persistor } from "./src/redux/store/store";
 import Navbar from "./src/components/Navbar";
+import { PersistGate } from 'redux-persist/integration/react';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <PaperProvider>
-        <NavigationContainer>
-          <Navbar />
-        </NavigationContainer>
-      </PaperProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <PaperProvider>
+          <NavigationContainer>
+            <Navbar />
+          </NavigationContainer>
+        </PaperProvider>
+      </PersistGate>
     </Provider>
   );
 }
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,18 +1,31 @@
 import { combineReducers, configureStore } from "@reduxjs/toolkit";
 // import { composeWithDevTools } from "redux-devtools-extension";
+import { persistStore, persistReducer } from "redux-persist"
 import listReducer from "../slices/slice-list";
 import authSlice from "../slices/auth-slice";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-let rootReducers = combineReducers({
+const persistConfig = {
+  key: "root",
+  storage: AsyncStorage,
+}
+
+const rootReducers = combineReducers({
   list: listReducer,
   auth: authSlice,
 });
 
-export const store = configureStore({
-  reducer: rootReducers,
+const persistedReducer = persistReducer(persistConfig, rootReducers)
+
+const store = configureStore({
+  reducer: persistedReducer,
   devTools: true,
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
       serializableCheck: false,
     }),
 });
+
+const persistor = persistStore(store);
+
+export { store, persistor };
