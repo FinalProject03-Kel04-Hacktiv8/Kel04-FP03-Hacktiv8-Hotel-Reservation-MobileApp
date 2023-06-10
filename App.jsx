0428eb20806f8d4a9480cb4e4eaa24fb42d562b4
ghaly09 <rachmatghaly0997@gmail.com>
2023-06-10 16:35:33 +0700

    Modified: update App.jsx for using Login page

--- App.jsx
+++ App.jsx
@@ -1,16 +1,18 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { PaperProvider } from "react-native-paper";
-import { Provider } from "react-redux";
+import { Provider, useSelector } from "react-redux";
 import { store, persistor } from "./src/redux/store/store";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 import Navbar from "./src/components/Navbar";
 import SearchDest from "./src/pages/Home/Search/SearchDest";
 import ListHotels from "./src/pages/Home/listHotels";
 import Detail from "./src/pages/Details/detail";
 import { BookingPage } from "./src/pages/Profile/bookingPage";
+import Login from "./src/pages/Login";
 
 export default function App() {
+  // const auth = useSelector((state) => state.auth);
   const Stack = createNativeStackNavigator();
   return (
     <Provider store={store}>
@@ -42,6 +44,11 @@ export default function App() {
                 <Stack.Screen
                   name="Booking"
                   component={BookingPage}
+                  options={{ headerShown: false }}
+                />
+                <Stack.Screen
+                  name="Login"
+                  component={Login}
                   options={{ headerShown: false }}
                 />
               </Stack.Group>
